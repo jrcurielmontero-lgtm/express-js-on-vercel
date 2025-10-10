7fca4cf6b1c333c97368524fe47a846d59dbf337
jrcurielmontero-lgtm <jrcurielmontero@gmail.com>
2025-10-10 14:14:29 +0200

    Update sendToBrevo.js

--- api/sendToBrevo.js
+++ api/sendToBrevo.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   // Permitir CORS
-  res.setHeader("Access-Control-Allow-Origin", "https://psicoboost.es"); // tu dominio
+  res.setHeader("Access-Control-Allow-Origin", "https://psicoboost.es/?page_id=38"); // tu dominio
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, api-key");
 
