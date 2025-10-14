355b20ede203c365ba92da6c21aba67143dab956
jrcurielmontero-lgtm <jrcurielmontero@gmail.com>
2025-10-14 21:38:34 +0200

    Update sendToBrevo.js

--- api/sendToBrevo.js
+++ api/sendToBrevo.js
@@ -24,8 +24,8 @@ export default async function handler(req, res) {
         APELLIDOS,
         TELEFONO,
         TIPO_ENTIDAD,
-        ESPECIALIDAD: Array.isArray(ESPECIALIDAD) ? ESPECIALIDAD.join(", ") : ESPECIALIDAD,
-        USO_RRSS: Array.isArray(USO_RRSS) ? USO_RRSS.join(", ") : USO_RRSS,
+       // ESPECIALIDAD: Array.isArray(ESPECIALIDAD) ? ESPECIALIDAD.join(", ") : ESPECIALIDAD,
+       // USO_RRSS: Array.isArray(USO_RRSS) ? USO_RRSS.join(", ") : USO_RRSS,
         OBJETIVO
       },
       listIds: [2] // tu lista de Brevo
