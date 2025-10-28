696c14a2bf9a333eb422f7f5ebefcc711c43ee03
jrcurielmontero-lgtm <jrcurielmontero@gmail.com>
2025-10-28 13:08:53 +0100

    Update WebhookSendProposal_up.js

--- api/WebhookSendProposal_up.js
+++ api/WebhookSendProposal_up.js
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-import { sendProposalEmail } from "./_utils/sendProposalEmail.js";
+import { sendProposalEmail } from "./utilSendProposalEmail.js";
 
 export default async function handler(req, res) {
   console.log("=== WebhookSendProposal_up ejecutado ===");
